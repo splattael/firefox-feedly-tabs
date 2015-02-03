97369415e88d398e43d13bc911507d8709eac4ca
Peter Suschlik <peter@suschlik.de>
2015-02-03 22:36:52 +0100

    Non-working "mark as read"

--- data/feedly-tabs.js
+++ data/feedly-tabs.js
@@ -6,11 +6,29 @@ function eachFeedlyUrl(callback) {
   var links = document.querySelectorAll("div[" + link_attribute + "]");
   for (var i = 0, size = links.length; i < size; i++) {
     var link = links[i];
+    var entryid = link.getAttribute("data-entryid");
     var url = link.getAttribute(link_attribute);
-    callback(url);
+    callback(entryid, url);
   }
 }
 
-eachFeedlyUrl(function(url) {
-  self.port.emit("link", url);
+eachFeedlyUrl(function(entryid, url) {
+  self.port.emit("link", { entryid: entryid, url: url });
 });
+
+self.port.on("tab_closed", function(link) {
+  console.log("CLOSED: " + link.entryid);
+  var ids = [ link.entryid ];
+  console.log(ids);
+  console.log("YO!");
+  console.log("YO2!");
+  console.log(unsafeWindow);
+  console.log("YO3!");
+  var window = unsafeWindow;
+  console.log(window);
+  console.log(window.streets);
+  console.log(window.streets.service("reader"));
+  window.streets.service("reader").askMarkEntriesAsRead(ids);
+});
+
+
